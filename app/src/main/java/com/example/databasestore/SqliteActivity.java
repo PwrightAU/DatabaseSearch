c5f6e536196247c2abb73334ac90357db758a55e
PwrightAU <54339470+PwrightAU@users.noreply.github.com>
2020-04-17 20:27:41 -0500

    Adding SQL Statements

--- app/src/main/java/com/example/databasestore/SqliteActivity.java
+++ app/src/main/java/com/example/databasestore/SqliteActivity.java
@@ -8,6 +8,9 @@ import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
@@ -25,13 +28,15 @@ public class SqliteActivity extends AppCompatActivity {
     private TableLayout history;
     private TableRow row;
     private ScrollView scroller;
+    private EditText dateFromEdit, dateToEdit, amountFromEdit, amountToEdit;
+    private Button search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SQLiteDatabase db;
+        final SQLiteDatabase db;
         db = openOrCreateDatabase("TransactionHistory", MODE_PRIVATE, null);
 
         currBalance = findViewById(R.id.textView);
@@ -39,6 +44,11 @@ public class SqliteActivity extends AppCompatActivity {
         row = findViewById(R.id.tableRow);
         time = findViewById(R.id.time);
         scroller = findViewById(R.id.scrollView2);
+        dateFromEdit = findViewById(R.id.dateEdit);
+        dateToEdit = findViewById(R.id.dateEdit2);
+        amountFromEdit = findViewById(R.id.amountFrom);
+        amountToEdit = findViewById(R.id.amountTo);
+        search = findViewById(R.id.button2);
 
         //db.execSQL("Drop Table If Exists History");
 
@@ -62,19 +72,118 @@ public class SqliteActivity extends AppCompatActivity {
 
         long count = DatabaseUtils.queryNumEntries(db, "History");
 
-        String s[] = {"id", "date", "amount", "reason"};
-        Cursor a = db.query("History", s, null, null, null, null, null);
-        for(int i = 0; i < count; i++) {
+        //Cursor a = db.query("History", s, null, null, null, null, null);
+        /*for(int i = 0; i < count; i++) {
             a.moveToPosition(i);
             history.addView(createNewTextView2(a.getString(1), a.getString(2), a.getString(3)));
-        }
-        a.close();
+        }*/
+        //a.close();
 
-        //Cursor b = db.rawQuery("SELECT SUM(Amount) as Total FROM History", null);
-        //int balance = b.getInt(b.getColumnIndex("Total"));
+        search.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String dateFrom = dateFromEdit.getText().toString();
+                String dateTo = dateToEdit.getText().toString();
+                String amountFrom = amountFromEdit.getText().toString();
+                String amountTo = amountToEdit.getText().toString();
+                searchQuery(dateFrom, dateTo, amountFrom, amountTo, db);
+            }
+        });
 
         currBalance.setText("$1380.00");
-        //b.close();
+    }
+
+    public void ClearTable(){
+        int count = history.getChildCount();
+        for (int i = 1; i < count; i++) {
+            history.removeViewAt(1);
+        }
+    }
+
+    public void searchQuery(String dateFrom, String dateTo, String priceFrom, String priceTo, SQLiteDatabase db) {
+        ClearTable();
+        String s[] = {"id", "date", "amount", "reason"};
+        Cursor result = db.query("History", s, null, null, null, null, null);
+        Double priceFromDo = null;
+        Double priceToDo = null;
+        if (!priceFrom.isEmpty())
+        {
+            priceFromDo = Double.parseDouble(priceFrom);
+        }
+        if (!priceTo.isEmpty())
+        {
+            priceToDo = Double.parseDouble(priceTo);
+        }
+
+        if (priceFromDo != null && priceToDo == null && dateFrom.isEmpty() && dateTo.isEmpty())
+        {
+            result = db.rawQuery("SELECT * FROM History WHERE Amount >= " + priceFromDo, null);
+        }
+        else if (priceFromDo == null && priceToDo != null && dateFrom.isEmpty() && dateTo.isEmpty())
+        {
+            result = db.rawQuery("SELECT * FROM History WHERE Amount <= " + priceToDo, null);
+        }
+        else if (priceFromDo == null && priceToDo == null && !dateFrom.isEmpty() && dateTo.isEmpty())
+        {
+            result = db.rawQuery("SELECT * FROM History WHERE Date >= '" + dateFrom + "'", null);
+        }
+        else if (priceFromDo == null && priceToDo == null && dateFrom.isEmpty() && !dateTo.isEmpty())
+        {
+            result = db.rawQuery("SELECT * FROM History WHERE Date <= '" + dateTo + "'", null);
+        }
+        else if (priceFromDo != null && priceToDo != null && dateFrom.isEmpty() && dateTo.isEmpty())
+        {
+            result = db.rawQuery("SELECT * FROM History WHERE Amount >= " + priceFromDo + " AND Amount <= " + priceToDo, null);
+        }
+        else if (priceFromDo == null && priceToDo == null && !dateFrom.isEmpty() && !dateTo.isEmpty())
+        {
+            result = db.rawQuery("SELECT * FROM History WHERE Date >= '" + dateFrom + "' AND Date <= '" + dateTo + "'", null);
+        }
+        else if (priceFromDo != null && priceToDo == null && !dateFrom.isEmpty() && dateTo.isEmpty())
+        {
+            result = db.rawQuery("SELECT * FROM History WHERE Amount >= " + priceFromDo + " AND Date >= '" + dateFrom + "'", null);
+        }
+        else if (priceFromDo != null && priceToDo == null && dateFrom.isEmpty() && !dateTo.isEmpty())
+        {
+            result = db.rawQuery("SELECT * FROM History WHERE Amount >= " + priceFromDo + " AND Date <= '" + dateTo + "'", null);
+        }
+        else if (priceFromDo == null && priceToDo != null && !dateFrom.isEmpty() && dateTo.isEmpty())
+        {
+            result = db.rawQuery("SELECT * FROM History WHERE Amount <= " + priceToDo + " AND Date >= '" + dateFrom + "'", null);
+        }
+        else if (priceFromDo == null && priceToDo != null && dateFrom.isEmpty() &&!dateTo.isEmpty())
+        {
+            result = db.rawQuery("SELECT * FROM History WHERE Amount <= " + priceToDo + " AND Date <= '" + dateTo + "'", null);
+        }
+        else if (priceFromDo != null && priceToDo != null && !dateFrom.isEmpty() && dateTo.isEmpty())
+        {
+            result = db.rawQuery("SELECT * FROM History WHERE Amount >= " + priceFromDo + " AND Amount <= " +
+                    priceToDo + " AND Date >= '" + dateFrom + "'", null);
+        }
+        else if (priceFromDo != null && priceToDo != null && dateFrom.isEmpty() && !dateTo.isEmpty())
+        {
+            result = db.rawQuery("SELECT * FROM History WHERE Amount >= " + priceFromDo + " AND Amount <= " +
+                    priceToDo + " AND Date <= '" + dateTo + "'", null);
+        }
+        else if (priceFromDo == null && priceToDo != null && !dateFrom.isEmpty() && !dateTo.isEmpty())
+        {
+            result = db.rawQuery("SELECT * FROM History WHERE Amount <= " + priceToDo + " AND Date >= '" +
+                    dateFrom + "' AND Date <= '" + dateTo + "'", null);
+        }
+        else if (priceFromDo != null && priceToDo == null && !dateFrom.isEmpty() && !dateTo.isEmpty())
+        {
+            result = db.rawQuery("SELECT * FROM History WHERE Amount >= " + priceFromDo + " AND Date >= '" +
+                    dateFrom + "' AND Date <= '" + dateTo +"'", null);
+        }
+        else if (priceFromDo != null && priceToDo != null && dateFrom != null && dateTo != null)
+        {
+            result = db.rawQuery("SELECT * FROM History WHERE Amount >= " + priceFromDo + " AND Amount <= " + priceToDo + " AND Date >= '" +
+                    dateFrom + "' AND Date <= '" + dateTo + "'", null);
+        }
+
+        while(result.moveToNext()) {
+            history.addView(createNewTextView2(result.getString(1), result.getString(2), result.getString(3)));
+        }
+        result.close();
     }
 
     private TextView createNewTextView1(String added) {
